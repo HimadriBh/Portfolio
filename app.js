27f294bc8ed0c657c0b3869792c14b9e54fdb9f2
Himadri Bhargava <himadribhargava@gmail.com>
2019-06-02 19:49:42 +0530

    new deploy

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var express = require("express");
 var app = express();
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 app.get("/", (req, res)=>{
     res.render("index");
 });
 
-app.listen(4340, function(){
+app.listen(process.env.PORT || 8080, function(){
     console.log("server is listening at port 4340");
 });
